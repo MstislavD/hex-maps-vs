bd54c00025ba9734011f5eb13f24f689735135f0
Mstislav Dobrotvorskiy <155465158+MstislavD@users.noreply.github.com>
2025-01-23 16:55:31 +0300

    hex parents

--- HexMaps/HexGeometry/HexGrid.cs
+++ HexMaps/HexGeometry/HexGrid.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.Globalization;
 using System.Net.Http.Headers;
 using System.Numerics;
 using System.Runtime.CompilerServices;
@@ -17,15 +18,18 @@ namespace HexGeometry
             public static Point operator +(Point p1, Point p2) => new(p1.X + p2.X, p1.Y + p2.Y);
         }
 
-        public readonly struct Hex(Point[] points, int[] neighbors)
+        public readonly struct Hex(int index, Point[] points, int[] neighbors, int parent)
         {
+            public readonly int Index { get; } = index;
             public Point[] Points { get; } = points;
 
             public int[] Neighbors { get; } = neighbors;
 
+            public int Parent { get; } = parent;
+
             public override string ToString()
             {
-                return $"Neighbors: {string.Join(", ", Neighbors)}";
+                return $"Index: {Index}; Neighbors: {string.Join(", ", Neighbors)}; Parent: {Parent}";
             }
         }
 
@@ -41,8 +45,12 @@ namespace HexGeometry
         public double Width { get { return width; } }
         public double Height { get { return height; } }
 
+        public Hex GetHex(int index) => hexes[index];
+
         public IEnumerable<Hex> Hexes => hexes;
         public static HexGrid Create(int columns, int rows) => new(columns, rows);
+
+        public static HexGrid CreateChild(HexGrid grid) => new(grid.Columns * 2, grid.Rows * 2 - 1);
 
         HexGrid(int columns, int rows)
         {
@@ -69,30 +77,40 @@ namespace HexGeometry
      
             Point[] hex_points = Generate_hex_points(l_radius, s_radius);
 
-            bool shift = true;
+            bool shifted_row = false;
+            bool heir_hex = true;
+            int parent = 0;
 
-            for (int i = 0, count = 0; i < rows; i++, center_y += y_step, center_x -= width)
-            {
-                shift = !shift;
-                center_x += shift ? x_step : 0;
-
-                for (int j = 0; j < columns; j++, count++, center_x += x_step)
+            for (int i = 0, count = 0; i < rows; i++)
+            {                             
+                for (int j = 0; j < columns; j++)
                 {
                     Point center = new(center_x, center_y);
                     Point[] points = hex_points.Select(p => center + p).ToArray();
-                    int[] neighbors = get_neighbor_indices(n_indexes, shift, i, j, count);
-                    hexes[count] = new(points, neighbors);
+                    int[] neighbors = get_neighbor_indices(n_indexes, shifted_row, i, j, count);
+                    int p = !shifted_row && heir_hex ? parent++ : -1;
+
+                    hexes[count] = new(count++, points, neighbors, p);
+
+                    center_x += x_step;
+                    heir_hex = !heir_hex;
                 }
+
+                shifted_row = !shifted_row;
+                heir_hex = shifted_row ? heir_hex : !heir_hex;
+
+                center_x += -width + (shifted_row ? x_step : 0);
+                center_y += y_step;               
             }
         }
 
-        private int[] get_neighbor_indices(int[][] n_indexes, bool shift, int i, int j, int count)
+        private int[] get_neighbor_indices(int[][] n_indexes, bool shift_row, int i, int j, int count)
         {
-            int[] neighbors = n_indexes[shift ? 1 : 0].Select(i => i + count).ToArray();
+            int[] neighbors = n_indexes[shift_row ? 1 : 0].Select(i => i + count).ToArray();
             if (j == 0)
             {
                 neighbors[4] = -1;
-                if (!shift)
+                if (!shift_row)
                 {
                     neighbors[3] = neighbors[5] = -1;
                 }
@@ -100,7 +118,7 @@ namespace HexGeometry
             if (j == columns - 1)
             {
                 neighbors[1] = -1;
-                if (shift)
+                if (shift_row)
                 {
                     neighbors[0] = neighbors[2] = -1;
                 }
--- HexMaps/HexMapsApp/HexMapsForm.cs
+++ HexMaps/HexMapsApp/HexMapsForm.cs
@@ -15,8 +15,11 @@ namespace HexMapsApp
 
         private void HexMapsForm_Shown(object? sender, EventArgs e)
         {
-            HexGrid hexGrid = HexGrid.Create(30, 20);
-            Log($"{hexGrid} created");
+            Random rnd = new();
+            HexGrid parentGrid = HexGrid.Create(15, 10);
+            Color[] colors = parentGrid.Hexes.Select(h => random_color(rnd)).ToArray();
+
+            HexGrid hexGrid = HexGrid.CreateChild(parentGrid);
 
             float x_factor = (float)(ClientSize.Width / hexGrid.Width);
             float y_factor = (float)(ClientSize.Height / hexGrid.Height);
@@ -26,19 +29,27 @@ namespace HexMapsApp
             Graphics g = Graphics.FromImage(image);
             g.Clear(Color.White);
 
-            Random rnd = new Random();
-
-            int hex_index = rnd.Next(0, 30 * 20);
             foreach (HexGrid.Hex hex in hexGrid.Hexes)
             {
-                if (hex.Neighbors.Contains(hex_index))
-                {
-                    g.FillPolygon(Brushes.Red, hex.Points.Select(p => Screen_point(p, factor)).ToArray());
-                }
+
+                int parent = choose_parent(hexGrid, hex, rnd);
+                SolidBrush brush = new(colors[parent]);
+                g.FillPolygon(brush, hex.Points.Select(p => Screen_point(p, factor)).ToArray());
                 g.DrawPolygon(Pens.Black, hex.Points.Select(p => Screen_point(p, factor)).ToArray());
             }
 
             CreateGraphics().DrawImage(image, 0, 0);
+        }
+
+        static int choose_parent(HexGrid hexGrid, HexGrid.Hex hex, Random rnd)
+        {
+            if (hex.Parent > -1)
+            {
+                return hex.Parent;
+            }
+
+            int[] candidates = hex.Neighbors.Where(n => n > -1).Select(n => hexGrid.GetHex(n).Parent).Where(p => p > -1).ToArray();
+            return candidates[rnd.Next(candidates.Length)];
         }
 
         static PointF Screen_point(HexGrid.Point p, float factor) => new((float)p.X * factor, (float)p.Y * factor);
@@ -47,5 +58,7 @@ namespace HexMapsApp
         {
             System.Diagnostics.Debug.WriteLine(msg);
         }
+
+        static Color random_color(Random rnd) => Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
     }
 }
